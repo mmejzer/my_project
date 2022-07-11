e2fc44450470be13b22cb285487d0d660aa25093
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-07-11 15:20:12 +0300

    fixed bugs 2

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :articles
 
   get "/draft", to: "draft#index"
-  get 'draft/index'
 
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy'  
