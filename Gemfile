55baa262616adedfc3ca23013dc08d1170424819
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-07-12 16:35:30 +0300

    Add new pages and navigation bar

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.6'
 
 gem 'devise'
+gem 'kaminari'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :articles
 
   get "/draft", to: "draft#index"
+  get 'home/about_me', to: 'home#about_me'
+  get 'home/contact_with_me', to: 'home#contact_with_me'
 
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy'  
