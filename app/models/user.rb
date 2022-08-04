d29e886bfa916df765347d65c99506bf790355ad
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-08-04 15:51:43 +0300

    made changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness:true
   validates :password, presence: true, length: { minimum: 6 }
 end
