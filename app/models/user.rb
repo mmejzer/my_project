3429f76224385750db473d2cc52ce6788c103fc3
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-08-05 10:40:04 +0300

    made a check

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness:true
+  validates :email, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
