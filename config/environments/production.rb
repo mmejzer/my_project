22ff1551252c61cf2c2a790e0dde4f3350562b82
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-08-12 12:35:58 +0300

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = ENV.fetch('my_project_HOST')
+  config.action_mailer.default_url_options = ENV.fetch('HOST')
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
