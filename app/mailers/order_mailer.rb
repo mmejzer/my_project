8d24e3a0f2059aca27518af2057ca6af15277b90
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-08-12 14:13:42 +0300

    change

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class OrderMailer < ApplicationMailer
-  default to: ENV.fetch('GMAIL_USER', nil)
+  default to: ENV.fetch('GMAIL_USER')
 
   def contacts_mail(params)
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    @url = ENV.fetch('HOST', nil)
+    @url = ENV.fetch('HOST')
     mail(subject: "Rails my project #{@first_name} sent you a message")
   end
 end
