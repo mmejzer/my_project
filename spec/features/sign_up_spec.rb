b7e026fa98b62cd6bc86a43cb22b1fc7bc905403
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-09-02 09:51:56 +0300

    due to changes one of the test is outdated, so it was removed

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,12 +3,6 @@
 require 'rails_helper'
 
 RSpec.feature 'create user' do
-  scenario 'empty first_name, last_name, email and password' do
-    visit new_user_registration_path
-    click_button 'Sign up'
-    expect(page).to have_content('5 errors prohibited this user from being saved')
-  end
-
   scenario 'valid' do
     visit new_user_registration_path
     fill_in 'First name', with: Faker::Name.first_name
