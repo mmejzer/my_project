9616b79f04a9c5a069bc43ab042647dd40e3d594
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-09-07 12:12:43 +0300

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   get '/draft', to: 'draft#index'
   get 'home/about_me', to: 'home#about_me'
