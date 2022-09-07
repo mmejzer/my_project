f67f544296b8eb8d75f7f85f8ad301b85d00a90d
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-09-07 14:54:04 +0300

    fix ruboocop error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
@@ -6,14 +8,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-     @article = Article.find(params[:article_id])
-     @comment = @article.comments.find(params[:id])
-     @comment.destroy
-     redirect_to article_path(@article), status: :see_other
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
