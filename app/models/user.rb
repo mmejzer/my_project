66002e76387aaa734ce55199374dcb4657332ffa
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-09-08 16:30:52 +0300

    fixed user.rb bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password, length: { minimum: 6 }, on: :update, allow_blank: true
 end
