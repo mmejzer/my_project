dccc67da7939dfb3009070123f3e86cc0eec0046
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-09-19 12:40:45 +0300

    fixed deletion confirmation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash.notice = "Article '#{@article.title}' Deleted!"
+    flash.notice = "Article '#{@article.title}' deleted!"
     redirect_to root_path, status: :see_other
   end
 
