14bc3cb08bbbb6088ba816d12f1ed41847ddb09f
DominaPluviam <mmejzer@mail.ru>
2022-11-08 14:38:48 +0000

    trying fix mailer bag

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,6 +49,4 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: ENV.fetch('myproject_HOST', nil) }
-
-  config.action_mailer.delivery_method = :smtp
 end
