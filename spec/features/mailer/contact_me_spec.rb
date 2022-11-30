6a4f1d542d28eecc60c7a39703ddded0c5605b1d
mac-mini-121 <mac-mini-121@Mac-mini-mac-mini-121.local>
2022-11-30 14:59:08 +0300

    trying fix bag

--- spec/features/mailer/contact_me_spec.rb
+++ spec/features/mailer/contact_me_spec.rb
@@ -5,10 +5,12 @@ require 'rails_helper'
 describe 'contact_me' do
   scenario 'send message to owner' do
     visit contacts_url
+
     fill_in 'Name', with: 'Test'
     fill_in 'Email', with: 'test@test.com'
     fill_in 'Message', with: 'Test message'
     click_button 'Send'
+
     expect(page).to have_content 'Message sent'
   end
 end
